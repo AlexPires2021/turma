dceec89b7c5c5a454929338433ecb54e471264c6
Alex <alexpires557@gmail.com>
2020-11-13 09:02:51 +0000

    Ficheiros iniciais do exercicio Turma

---
+++ turma.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Turma de Programação Web</title>
+</head>
+<body>
+    <header>
+        <h1>Turma de Programação Web</h1>
+        <h2>TPSI</h2>
+    </header>
+
+    <main>
+        <ul>
+            <li>
+                <div class="foto">
+                    <img src="https://siesgt.ipsantarem.pt/esgt/fotografias_service.foto?pct_cod=190100249">
+                </div>
+                <div class="info">
+                    <span class="nome">Alex Ferreira Pires</span>
+                    <span class="numero">190100249</span>
+                </div>    
+            </li>
+        </ul>
+    </main>
+</body>
+</html>
